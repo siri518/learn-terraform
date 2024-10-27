d2a7af1d852993dfe18b4997d153fee7f2d46469
SIRI <sireeshasiri946@gmail.com>
2024-10-27 18:40:35 +0530

    adding meta file

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -11,3 +11,48 @@ output "URL" {
 output "URL1" {
   value = "URL - ${var.URL}"
 }
+
+# Data Types
+variable  "num1" {
+  default = 10
+}
+
+variable  "bool1" {
+  default = true
+}
+
+variable "str1" {
+  default = "hello"
+}
+
+variable "str2" {
+  default = "World"
+}
+
+variable "list1"{
+  default = [
+  10, 20, "xyz",false
+  ]
+}
+
+variable "map1"{
+   default = {
+     aws ={
+       trainer = "john"
+       duration = 30
+     }
+
+     devops = {
+       trainer = "raghu"
+       duration = 20
+     }
+   }
+}
+
+output "list_1"{
+  value = var.list1[2]
+}
+
+output "map1_aws_trainer"{
+  value = var.map1["aws"]["trainer"]
+}
