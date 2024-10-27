c363d1a6f1c8c41a5cd39d92773d01fc9f140da3
SIRI <sireeshasiri946@gmail.com>
2024-10-27 18:07:11 +0530

    adding meta file

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -7,6 +7,31 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z10227673MRFB0EY34RV8"
+  name    = "frontend-dev.venkysiridevops.online"
+  type    = "NS"
+  ttl     = "30"
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids =["sg-01afef600657f35e7"]
+  tags = {
+    Name = "workstation"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z10227673MRFB0EY34RV8"
+  name    = "mongodb-dev.venkysiridevops.online"
+  type    = "NS"
+  ttl     = "30"
+  records = [aws_instance.mongodb.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -14,6 +39,14 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue"
   }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z10227673MRFB0EY34RV8"
+  name    = "catalogue-dev.venkysiridevops.online"
+  type    = "NS"
+  ttl     = "30"
+  records = [aws_instance.catalogue.private_ip]
 }
 
 resource "aws_instance" "redis" {
@@ -25,6 +58,14 @@ resource "aws_instance" "redis" {
   }
 }
 
+resource "aws_route53_record" "redis" {
+  zone_id = "Z10227673MRFB0EY34RV8"
+  name    = "redis-dev.venkysiridevops.online"
+  type    = "NS"
+  ttl     = "30"
+  records = [aws_instance.redis.private_ip]
+}
+
 resource "aws_instance" "user" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -32,6 +73,14 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z10227673MRFB0EY34RV8"
+  name    = "user-dev.venkysiridevops.online"
+  type    = "NS"
+  ttl     = "30"
+  records = [aws_instance.user.private_ip]
 }
 
 resource "aws_instance" "cart" {
@@ -43,6 +92,14 @@ resource "aws_instance" "cart" {
   }
 }
 
+resource "aws_route53_record" "cart" {
+  zone_id = "Z10227673MRFB0EY34RV8"
+  name    = "cart-dev.venkysiridevops.online"
+  type    = "NS"
+  ttl     = "30"
+  records = [aws_instance.cart.private_ip]
+}
+
 resource "aws_instance" "mysql" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -50,6 +107,14 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql"
   }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z10227673MRFB0EY34RV8"
+  name    = "mysql-dev.venkysiridevops.online"
+  type    = "NS"
+  ttl     = "30"
+  records = [aws_instance.mysql.private_ip]
 }
 
 resource "aws_instance" "shipping" {
@@ -61,6 +126,14 @@ resource "aws_instance" "shipping" {
   }
 }
 
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z10227673MRFB0EY34RV8"
+  name    = "shipping-dev.venkysiridevops.online"
+  type    = "NS"
+  ttl     = "30"
+  records = [aws_instance.shipping.private_ip]
+}
+
 resource "aws_instance" "rabbitmq" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -68,6 +141,14 @@ resource "aws_instance" "rabbitmq" {
   tags = {
     Name = "rabbitmq"
   }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z10227673MRFB0EY34RV8"
+  name    = "rabbitmq-dev.venkysiridevops.online"
+  type    = "NS"
+  ttl     = "30"
+  records = [aws_instance.rabbitmq.private_ip]
 }
 
 resource "aws_instance" "payment" {
@@ -79,5 +160,12 @@ resource "aws_instance" "payment" {
   }
 }
 
+resource "aws_route53_record" "payment" {
+  zone_id = "Z10227673MRFB0EY34RV8"
+  name    = "payment-dev.venkysiridevops.online"
+  type    = "NS"
+  ttl     = "30"
+  records = [aws_instance.payment.private_ip]
+}
 
 
