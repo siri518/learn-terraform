742e8368bddf774e1763b5cb3209c24925d21c4e
SIRI <sireeshasiri946@gmail.com>
2024-11-01 22:15:27 +0530

    adding meta file

--- 06-modules/main.tf
+++ 06-modules/main.tf
@@ -15,9 +15,9 @@ output "ip" {
   value = module.ec2
 }
 
-# module "route53" {
-#   count = length(var.instances)
-#   source = "./route53"
-#   instance_name = var.instances[count.index]
-#   ip_address = module.ec2.ip_address[count.index]
-# }
+module "route53" {
+  count = length(var.instances)
+  source = "./route53"
+  instance_name = var.instances[count.index]
+  ip_address = module.ec2[count.index].ip_address
+}
