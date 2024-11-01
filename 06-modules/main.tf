a0bc0adc855ee2df8e46543ae10207548c3fb43c
SIRI <sireeshasiri946@gmail.com>
2024-11-01 08:42:37 +0530

    adding meta file

--- 06-modules/main.tf
+++ 06-modules/main.tf
@@ -1,13 +1,14 @@
 variable "instances" {
   default = [
-  "test1",
- "test2" ]
+    "test1",
+    "test2"
+  ]
 }
 
 module "ec2" {
   count = length(var.instances)
   source = "./ec2"
-  name = var.instances[count.index]
+  instance_name = var.instances[count.index]
 }
 
 # module "route53" {
