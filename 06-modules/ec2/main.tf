a73b0aa8ecc1420158c763017378ab0e652ecdb7
SIRI <sireeshasiri946@gmail.com>
2024-11-01 08:28:41 +0530

    adding meta file

---
+++ 06-modules/ec2/main.tf
@@ -0,0 +1,12 @@
+
+resource "aws_instance" "instance" {
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-01afef600657f35e7"]
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+variable "instance_name" {}
+
