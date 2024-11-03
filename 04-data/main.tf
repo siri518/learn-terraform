17f3c2990d6b1f7bba911108448b05f14fd73a5b
SIRI <sireeshasiri946@gmail.com>
2024-11-03 11:46:26 +0530

    adding meta file

--- 04-data/main.tf
+++ 04-data/main.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "test" {
+  ami = data.aws_ami.ami.id
   instance_type = "t3.small"
   vpc_security_group_ids =["sg-01afef600657f35e7"]
   tags = {
@@ -6,3 +7,12 @@ resource "aws_instance" "test" {
   }
 }
 
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "RHEL-9-DevOps-Practice"
+  owners      = [973714476881]
+}
+
+data "aws_security_group" "sg" {
+  name = "allow-all"
+}
